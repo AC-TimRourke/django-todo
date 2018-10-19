3fad369a0f0fdc122023d26cc47dd9589a7ce037
Tim Rourke <trourke@activecampaign.com>
2018-10-19 14:21:53 -0500

    Add Todo model definition
    Add Todo model definition

--- project/todo/models.py
+++ project/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Todo(models.Model):
+    description = models.CharField(max_length=250)
+    created_at  = models.DateTimeField()
+    updated_at  = models.DateTimeField()
+    finished_at = models.DateTimeField()
+    user_id     = models.ForeignKey(User, on_delete=models.CASCADE)
